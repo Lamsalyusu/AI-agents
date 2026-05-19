57e233530b9e0d62bc4b849ceeb0b55f38f82fba
Yuyutsu Lamsal <yuyutsulamsal@users.noreply.huggingface.co>
2026-05-19 11:50:24 +0000

    Update app.py
    Updated tool function usd_to_npr

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from smolagents import CodeAgent,DuckDuckGoSearchTool, HfApiModel,load_tool,tool
+from smolagents import CodeAgent,DuckDuckGoSearchTool,InferenceClientModel,load_tool,tool
 import datetime
 import requests
 import pytz
@@ -9,14 +9,20 @@ from Gradio_UI import GradioUI
 
 # Below is an example of a tool that does nothing. Amaze us with your creativity !
 @tool
-def my_custom_tool(arg1:str, arg2:int)-> str: #it's import to specify the return type
+def usd_to_npr(usd:float)-> str: #it's import to specify the return type
     #Keep this format for the description / args / args description but feel free to modify the tool
-    """A tool that does nothing yet 
+    """A tool that takes usdt value and converts it into nepali rupees using the current exchange rates
     Args:
-        arg1: the first argument
-        arg2: the second argument
+        usd: The first argument which is the amount in US Dollars to convert
     """
-    return "What magic will you build ?"
+    try:
+        response = requests.get("https://open.er-api.com/v6/latest/USD",timeout = 10)
+        data = response.json()
+        rate = data["rates"]["NPR"]
+        npr = usd * rate
+        return f"{usd} USD equals {npr:.2f} live rate conersion 1 USD = {rate:.2f} NPR"
+    except Exception as e:
+        return f"Error fetching exchange data rates: {str(e)}"
 
 @tool
 def get_current_time_in_timezone(timezone: str) -> str:
@@ -39,7 +45,7 @@ final_answer = FinalAnswerTool()
 # If the agent does not answer, the model is overloaded, please use another model or the following Hugging Face Endpoint that also contains qwen2.5 coder:
 # model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
 
-model = HfApiModel(
+model = InferenceClientModel(
 max_tokens=2096,
 temperature=0.5,
 model_id='Qwen/Qwen2.5-Coder-32B-Instruct',# it is possible that this model may be overloaded
@@ -55,7 +61,7 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[final_answer,usd_to_npr,get_current_time_in_timezone], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
